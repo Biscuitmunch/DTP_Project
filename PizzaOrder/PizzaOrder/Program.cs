28c4421c5cc9d8a400270e9a35486f53b25be460
Biscuitmunch <biscuitmunch0@gmail.com>
2018-05-08 09:32:45 +1200

    Add files via upload

---
+++ PizzaOrder/PizzaOrder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is the order for delivery or pickup?");
+            string choice = Console.ReadLine();
+            if ((choice == "delivery") || (choice == "Delivery") || (choice == "1") || (choice == "d") || (choice == "D"))
+            {
+
+            }
+        }
+    }
+}
